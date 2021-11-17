08f97b2d2c1958696709c8e3f347a293f7d98a05
Hyeonyee <jgh10070@naver.com>
2021-11-17 22:19:43 +0900

    [FIX] src 경로 재 수정

--- src/data/modules/redux/middleware/schedule/index.ts
+++ src/data/modules/redux/middleware/schedule/index.ts
@@ -1,7 +1,7 @@
 import { takeLatest } from 'redux-saga/effects';
-import createRequestSaga from '../../../../../utils/saga/createRequestSaga';
-import { getScheduleApi, updateScheduleApi } from '../../../../api/index';
-import { GET_STATUS, SET_SCHEDULE } from '../../../../modules/redux/action/schedule/interface';
+import createRequestSaga from 'src/utils/saga/createRequestSaga';
+import { getScheduleApi, updateScheduleApi } from 'src/data/api/index';
+import { GET_STATUS, SET_SCHEDULE } from 'src/data/modules/redux/action/schedule/interface';
 
 export const getScheduleSaga = createRequestSaga(GET_STATUS, getScheduleApi);
 export const updateScheduleSaga = createRequestSaga(SET_SCHEDULE, updateScheduleApi);

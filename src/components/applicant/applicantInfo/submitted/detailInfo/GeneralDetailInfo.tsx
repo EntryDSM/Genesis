8955d04433878d5648cc9f855de802083d390d39
Hyeonyee <jgh10070@naver.com>
2021-12-02 23:45:39 +0900

    [FIX] GDetailInfo Props 구조 수정

--- src/components/applicant/applicantInfo/submitted/detailInfo/GeneralDetailInfo.tsx
+++ src/components/applicant/applicantInfo/submitted/detailInfo/GeneralDetailInfo.tsx
@@ -1,33 +1,33 @@
 import React, { FC } from "react";
 
 import * as S from "./style";
-import {
-  ApplicantPersonalData,
-  ApplicantEvaluation,
-} from "src/data/api/apiTypes";
 import { phoneWithHyphen } from "src/utils/checkType";
 
 interface Props {
-  applicantPersonalData: ApplicantPersonalData;
-  applicantEvaluation: ApplicantEvaluation;
+  applicantCommonInformation: {
+    name: string;
+    school_name: string;
+    email: string;
+    telephone_number: string;
+    school_tel?: string;
+    parent_tel: string;
+  };
+  applicantEvaluation: {
+    volunteer_time: number;
+    conversion_score: number;
+    day_absence_count: number;
+    lecture_absence_count: number;
+    early_leave_count: number;
+    lateness_count: number;
+    self_introduce: string;
+    study_plan: string;
+    average_score: number;
+  };
 }
 
 const DetailInfo: FC<Props> = ({
-  applicantPersonalData: {
-    telephone_number,
-    parent_tel,
-    school_tel,
-    email,
-    home_tel,
-  },
-  applicantEvaluation: {
-    volunteer_time,
-    conversion_score,
-    day_absence_count,
-    lecture_absence_count,
-    early_leave_count,
-    lateness_count,
-  },
+  applicantCommonInformation,
+  applicantEvaluation,
 }) => {
   const setContactForm = React.useCallback((contact: string) => {
     return phoneWithHyphen(contact);
@@ -38,39 +38,39 @@ const DetailInfo: FC<Props> = ({
       <tbody>
         <tr>
           <th>이메일</th>
-          <td>{email}</td>
+          <td>{applicantCommonInformation.email}</td>
         </tr>
         <tr>
           <th>학생 연락처</th>
-          <td>{setContactForm(telephone_number)}</td>
+          <td>{setContactForm(applicantCommonInformation.telephone_number)}</td>
           <th>부모님 연락처</th>
-          <td>{setContactForm(parent_tel)}</td>
+          <td>{setContactForm(applicantCommonInformation.parent_tel)}</td>
         </tr>
         <tr>
           <th>학교 연락처</th>
-          <td>{setContactForm(school_tel)}</td>
-          <th>자택 연락처</th>
-          <td>{setContactForm(home_tel)}</td>
+          <td>{setContactForm(applicantCommonInformation.school_tel)}</td>
+          {/* <th>자택 연락처</th>
+          <td>{setContactForm(applicantCommonInformation.)}</td> */}
         </tr>
         <tr></tr>
         <tr>
           <th>봉사시간</th>
-          <td>{volunteer_time || 0} 시간</td>
+          <td>{applicantEvaluation.volunteer_time || 0} 시간</td>
           <th>성적 점수</th>
-          <td>{conversion_score || 0} 점</td>
+          <td>{applicantEvaluation.conversion_score || 0} 점</td>
         </tr>
         <tr>
           <th rowSpan={2}>출석정보</th>
           <td>
             <div>
               <span>전체 무단 결석 일수</span>
-              <span>{day_absence_count || 0}</span>
+              <span>{applicantEvaluation.day_absence_count || 0}</span>
             </div>
           </td>
           <td colSpan={2}>
             <div>
               <span>전체 무단 조퇴 일수</span>
-              <span>{early_leave_count || 0}</span>
+              <span>{applicantEvaluation.early_leave_count || 0}</span>
             </div>
           </td>
         </tr>
@@ -78,13 +78,13 @@ const DetailInfo: FC<Props> = ({
           <td>
             <div>
               <span>전체 무단 지각 일수</span>
-              <span>{lateness_count || 0}</span>
+              <span>{applicantEvaluation.lateness_count || 0}</span>
             </div>
           </td>
           <td colSpan={2}>
             <div>
               <span>전체 무단 결과 일수</span>
-              <span>{lecture_absence_count || 0}</span>
+              <span>{applicantEvaluation.lecture_absence_count || 0}</span>
             </div>
           </td>
         </tr>

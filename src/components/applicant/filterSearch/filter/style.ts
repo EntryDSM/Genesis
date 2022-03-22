3bc5bef1087647bb92fadee27b4e75a22127ead3
Jeon gyuhyun <jgh10070@naver.com>
2022-03-22 10:01:25 +0900

    [UPDATE] 모달의 on/off에 따른 ui 개편화 작업

--- src/components/applicant/filterSearch/filter/style.ts
+++ src/components/applicant/filterSearch/filter/style.ts
@@ -1,29 +1,66 @@
 import { pxToRem } from 'src/styles';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
-export const FilterWrapper = styled.div`
-  width: 95%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 0 2.5%;
-  margin-top: 10px;
+export const FilterWrapper = styled.div<{ isContainerWidth: boolean }>`
+  ${({isContainerWidth}) => isContainerWidth ?
+    css`
+      width: 95%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-between;
+      margin: 0 2.5%;
+      margin-top: 10px;
+    `:
+    css`
+      width: 95%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 0 2.5%;
+      margin-top: 10px;
+    `
+  }
 `;
 
-export const FilterSelectBox = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 68%;
-  min-width: ${pxToRem(752)}rem;
+export const FilterSelectBox = styled.div<{ isContainerWidth: boolean }>`
+  ${({isContainerWidth}) => isContainerWidth ?
+    css`
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      min-width: ${pxToRem(752)}rem;
+    `:
+    css`
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 68%;
+      min-width: ${pxToRem(752)}rem;
+    `
+  }
 `;
 
-export const FilterButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  width: 32%;
-  min-width: ${pxToRem(320)}rem;
+export const FilterButtonContainer = styled.div<{ isContainerWidth: boolean }>`
+  ${({isContainerWidth}) => isContainerWidth ?
+    css`
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      width: 100%;
+      min-width: ${pxToRem(320)}rem;
+      margin-top: 10px;
+    `:
+    css`
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      width: 32%;
+      min-width: ${pxToRem(320)}rem;
+      margin-top: 10px;
+    `
+  }
 `;
 
 export const FilterItemContainer = styled.span`

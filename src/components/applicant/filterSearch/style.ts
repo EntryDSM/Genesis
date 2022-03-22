049ef4f2a04b4bf89c541451b300e010f2243089
Jeon gyuhyun <jgh10070@naver.com>
2022-03-22 10:01:20 +0900

    [UPDATE] 모달의 on/off에 따른 ui 개편화 작업

--- src/components/applicant/filterSearch/style.ts
+++ src/components/applicant/filterSearch/style.ts
@@ -1,14 +1,28 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
-const FilterSearchWrapper = styled.div`
-  width: 100%;
-  height: 14%;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 10px;
-  box-sizing: border-box;
-  justify-content: flex-end;
-  position: relative;
+const FilterSearchWrapper = styled.div<{ isContainerWidth: boolean }>`
+  ${({isContainerWidth}) => isContainerWidth ?
+    css`
+      width: 100%;
+      height: 18%;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 10px;
+      box-sizing: border-box;
+      justify-content: flex-end;
+      position: relative;
+    `:
+    css`
+      width: 100%;
+      height: 14%;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 10px;
+      box-sizing: border-box;
+      justify-content: flex-end;
+      position: relative;
+    `
+  }
 `;
 
 export default FilterSearchWrapper;

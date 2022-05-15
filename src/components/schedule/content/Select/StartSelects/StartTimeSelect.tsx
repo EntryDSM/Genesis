ad96b24c312fe90cca85f7a0ea4f279266a48d8b
Jeon gyuhyun <jgh10070@naver.com>
2022-05-15 09:55:49 +0900

    [UPDATE] startTimeSelect

---
+++ src/components/schedule/content/Select/StartSelects/StartTimeSelect.tsx
@@ -0,0 +1,63 @@
+import React, { FC } from "react";
+import * as S from "../../../style";
+import { select_off, select_on } from "src/assets/schedule";
+import { processTimeType } from "src/data/modules/redux/reducer/schedule/interface";
+import { START_DATE } from "src/data/modules/redux/reducer/schedule/scheduleConstance";
+
+interface Props {
+  date: Array<processTimeType>;
+  setStartScheduleDay: (payload: number) => void;
+}
+
+const TimeSelect: FC<Props> = ({ date, setStartScheduleDay }) => {
+  const [active, setActive] = React.useState(false);
+  const [disabled, setDisabled] = React.useState("normal");
+  const onestToThirtyOnest = [...Array(31)].map((_, i) => i + 1);
+
+  const onSelectClick = () => {
+    if (active === true) {
+      setActive(false);
+      setDisabled("normal");
+    } else {
+      setActive(true);
+      setDisabled("enabled");
+    }
+  };
+
+  const onSelectDayClick = (e) => {
+    const day = e.target.innerText;
+    day < 10 ? setStartScheduleDay(0 + day) : setStartScheduleDay(day);
+    localStorage.setItem("startScheduleDay", JSON.stringify(day));
+  };
+
+  const getLocalStorage =
+    JSON.parse(localStorage.getItem("startScheduleDay")) || "18";
+
+  const activeImg = React.useMemo(() => {
+    if (active) return <img src={select_on} alt="select_on" />;
+    else return <img src={select_off} alt="select_off" />;
+  }, [active]);
+
+  return (
+    <S.Select disabled={disabled} onClick={onSelectClick}>
+      <S.SelectContent>
+        <p>
+          {date
+            .filter((date) => date.type === START_DATE)[0]
+            .date.slice(8, 10) && getLocalStorage}
+        </p>
+        {activeImg}
+      </S.SelectContent>
+      {active && (
+        <S.SubSelect>
+          <S.GrayLine width={52} />
+          {onestToThirtyOnest.map((day) => {
+            return <p onClick={onSelectDayClick}>{day}</p>;
+          })}
+        </S.SubSelect>
+      )}
+    </S.Select>
+  );
+};
+
+export default TimeSelect;

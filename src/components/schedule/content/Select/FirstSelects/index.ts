70d6819eed0225d86a1c7d0aa1c48e65984fc0b8
Jeon gyuhyun <jgh10070@naver.com>
2022-05-17 12:06:51 +0900

    [UPDATE] Select export module 수정

--- src/components/schedule/content/Select/FirstSelects/index.ts
+++ src/components/schedule/content/Select/FirstSelects/index.ts
@@ -1,2 +1,5 @@
 export { default as FirstMonthSelect } from './FirstMonthSelect';
 export { default as FirstDaySelect } from './FirstDaySelect';
+export { default as FirstHourSelect } from './FirstHourSelect';
+export { default as FirstMinuteSelect } from './FirstMinuteSelect';
+export { default as FirstSecondSelect } from './FirstSecondSelect';
--- src/components/schedule/content/Select/SecondSelects/index.ts
+++ src/components/schedule/content/Select/SecondSelects/index.ts
@@ -1,2 +1,5 @@
 export { default as SecondMonthSelect } from './SecondMonthSelect';
 export { default as SecondDaySelect } from './SecondDaySelect';
+export { default as SecondHourSelect } from './SecondHourSelect';
+export { default as SecondMinuteSelect } from './SecondMinuteSelect';
+export { default as SecondSecondSelect } from './SecondSecondSelect';

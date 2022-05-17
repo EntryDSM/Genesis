6f2047c25619fe19e3875c8d2a5f6e54f7289cd1
Jeon gyuhyun <jgh10070@naver.com>
2022-05-17 12:05:43 +0900

    [ADD] MonthSelect

--- src/components/schedule/content/Select/FirstSelects/FirstMonthSelect.tsx
+++ src/components/schedule/content/Select/FirstSelects/FirstMonthSelect.tsx
@@ -3,16 +3,22 @@ import * as S from "../../../style";
 import { select_off, select_on } from "../../../../../assets/schedule";
 import { processTimeType } from "../../../../../data/modules/redux/reducer/schedule/interface";
 import { FIRST_ANNOUNCEMENT } from "src/data/modules/redux/reducer/schedule/scheduleConstance";
+import { useSchedule } from "src/hooks/schedule";
 
 interface Props {
   date: Array<processTimeType>;
-  setFirstScheduleMonth: (payload: number) => void;
+  setFirstScheduleMonth: (payload: string) => void;
 }
 
 const MonthSelect: FC<Props> = ({ date, setFirstScheduleMonth }) => {
+  const { setState } = useSchedule();
   const [active, setActive] = React.useState(false);
   const [disabled, setDisabled] = React.useState("normal");
   const JanuaryToDecember = [...Array(12)].map((_, i) => i + 1);
+
+  React.useEffect(() => {
+    setState.getStatus();
+  }, []);
 
   const onSelectClick = () => {
     if (active === true) {
@@ -33,13 +39,31 @@ const MonthSelect: FC<Props> = ({ date, setFirstScheduleMonth }) => {
   };
 
   const getLocalStorage = JSON.parse(
-    localStorage.getItem("firstScheduleMonth") || "3"
+    localStorage.getItem("firstScheduleMonth") ||
+      JSON.stringify(
+        !date
+          .filter((date) => date.type === FIRST_ANNOUNCEMENT)[0]
+          .date.slice(5, 7)
+      )
   );
 
   const activeImg = React.useMemo(() => {
     if (active) return <img src={select_on} alt="select_on" />;
     else return <img src={select_off} alt="select_off" />;
   }, [active]);
+
+  React.useEffect(() => {
+    localStorage.setItem(
+      "firstScheduleMonth",
+      JSON.stringify(
+        date
+          .filter(
+            (date) => date.type === FIRST_ANNOUNCEMENT && date.date.slice(5, 7)
+          )[0]
+          .date.slice(5, 7)
+      )
+    );
+  }, []);
 
   return (
     <S.Select disabled={disabled} onClick={onSelectClick}>
--- src/components/schedule/content/Select/StartSelects/StartMonthSelect.tsx
+++ src/components/schedule/content/Select/StartSelects/StartMonthSelect.tsx
@@ -3,16 +3,22 @@ import * as S from "../../../style";
 import { select_off, select_on } from "src/assets/schedule";
 import { processTimeType } from "src/data/modules/redux/reducer/schedule/interface";
 import { START_DATE } from "src/data/modules/redux/reducer/schedule/scheduleConstance";
+import { useSchedule } from "src/hooks/schedule";
 
 interface Props {
   date: Array<processTimeType>;
-  setStartScheduleMonth: (payload: number) => void;
+  setStartScheduleMonth: (payload: string) => void;
 }
 
 const MonthSelect: FC<Props> = ({ date, setStartScheduleMonth }) => {
+  const { setState } = useSchedule();
   const [active, setActive] = React.useState(false);
   const [disabled, setDisabled] = React.useState("normal");
   const JanuaryToDecember = [...Array(12)].map((_, i) => i + 1);
+
+  React.useEffect(() => {
+    setState.getStatus();
+  }, []);
 
   const onSelectClick = () => {
     if (active === true) {
@@ -33,7 +39,10 @@ const MonthSelect: FC<Props> = ({ date, setStartScheduleMonth }) => {
   };
 
   const getLocalStorage = JSON.parse(
-    localStorage.getItem("startScheduleMonth") || "3"
+    localStorage.getItem("startScheduleMonth") ||
+      JSON.stringify(
+        !date.filter((date) => date.type === START_DATE)[0].date.slice(5, 7)
+      )
   );
 
   const activeImg = React.useMemo(() => {
@@ -41,14 +50,23 @@ const MonthSelect: FC<Props> = ({ date, setStartScheduleMonth }) => {
     else return <img src={select_off} alt="select_off" />;
   }, [active]);
 
+  React.useEffect(() => {
+    localStorage.setItem(
+      "startScheduleMonth",
+      JSON.stringify(
+        date
+          .filter(
+            (date) => date.type === START_DATE && date.date.slice(5, 7)
+          )[0]
+          .date.slice(5, 7)
+      )
+    );
+  }, []);
+
   return (
     <S.Select disabled={disabled} onClick={onSelectClick}>
       <S.SelectContent>
-        <p>
-          {date
-            .filter((date) => date.type === START_DATE)[0]
-            .date.slice(5, 7) && getLocalStorage}
-        </p>
+        <p>{getLocalStorage}</p>
         {activeImg}
       </S.SelectContent>
       {active && (

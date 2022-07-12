76bf16d87405b713bd07b82a3e3f80c02b5391bf
강석현 <tjrgus0713@gmail.com>
2022-07-12 14:49:31 +0900

    [UPDATE] 애니메이션 속도 수정

--- genesis/src/components/applicant/style.ts
+++ genesis/src/components/applicant/style.ts
@@ -2,48 +2,46 @@ import styled, { css } from "styled-components";
 import { color, pxToRem } from "../../styles";
 
 export const Applicant = styled.div<{ isContainerWidth?: boolean }>`
-  ${({isContainerWidth}) => 
-    isContainerWidth ?
-      css`
-        position: relative;
-        width: 100%;
-        min-width: ${pxToRem(1136)}rem;
-        min-height: 96vh;
-        display: flex;
-        background: ${color.backgorund};
-        box-sizing: border-box;
-        padding-top: 75px;
-        overflow: hidden;
-      ` : 
-      css`
-        width: 80%;
-        min-width: ${pxToRem(1136)}rem;
-        min-height: 96vh;
-        display: flex;
-        background: ${color.backgorund};
-        box-sizing: border-box;
-        padding-top: 75px;
-        margin: 0 auto;
-        transition: all ease 1.3s;
-        overflow: hidden;
-      ` 
-  }
+  ${({ isContainerWidth }) =>
+    isContainerWidth
+      ? css`
+          position: relative;
+          width: 100%;
+          min-width: ${pxToRem(1136)}rem;
+          min-height: 96vh;
+          display: flex;
+          background: ${color.backgorund};
+          box-sizing: border-box;
+          padding-top: 75px;
+          overflow: hidden;
+        `
+      : css`
+          width: 80%;
+          min-width: ${pxToRem(1136)}rem;
+          min-height: 96vh;
+          display: flex;
+          background: ${color.backgorund};
+          box-sizing: border-box;
+          padding-top: 75px;
+          margin: 0 auto;
+          transition: all ease 0.5s;
+          overflow: hidden;
+        `}
 `;
 
 export const ApplicantContainer = styled.div<{ isContainerWidth?: boolean }>`
   display: flex;
   flex-direction: column;
-  ${({ isContainerWidth }) => 
-    isContainerWidth ?
-      css`
-        width: 60%;
-        transition: all ease 1.3s;
-      ` : 
-      css`
-        width: 100%;
-        transition: all ease 1.3s;
-      `  
-  };
+  ${({ isContainerWidth }) =>
+    isContainerWidth
+      ? css`
+          width: 60%;
+          transition: all ease 0.5s;
+        `
+      : css`
+          width: 100%;
+          transition: all ease 0.5s;
+        `};
 `;
 
 export const PaginationBox = styled.div`
@@ -55,22 +53,21 @@ export const PaginationBox = styled.div`
 `;
 
 export const ApplicantInfoWrap = styled.div<{
-    isContainerWidth?: boolean, 
-  }>`
+  isContainerWidth?: boolean;
+}>`
   position: absolute;
   z-index: 1;
   top: 60px;
   right: 0;
-  ${({ isContainerWidth }) => 
-    isContainerWidth ?
-      css`
-        width: 40%;
-        transition: all ease 1.2s;
-      ` : 
-      css`
-        width: 0%;
-        transition: all ease 1.2s;
-      `  
-  };
+  ${({ isContainerWidth }) =>
+    isContainerWidth
+      ? css`
+          width: 40%;
+          transition: all ease 0.5s;
+        `
+      : css`
+          width: 0%;
+          transition: all ease 0.5s;
+        `};
   height: 100%;
 `;

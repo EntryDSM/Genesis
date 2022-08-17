5e1baab690dc2b0e44830c6235448ce6db11fefe
강석현 <tjrgus0713@gmail.com>
2022-08-17 14:34:32 +0900

    [FIX] : 사소한 수정

--- genesis/src/data/api/schedule/scheduleApi.ts
+++ genesis/src/data/api/schedule/scheduleApi.ts
@@ -1,22 +1,23 @@
-import * as T from '../apiTypes';
+import * as T from "../apiTypes";
 import { getAccessToken } from "src/utils/token";
 import { getRequest, authorization } from "../default/default";
 import uri from "../uri";
 
 export const getScheduleApi = async () => {
-    const response = await getRequest().get<T.GetSchedulesResponse>(uri.schedule);
-  
-    return response;
-  }
-  
-export const updateScheduleApi = async (access_token: string, payload: T.UpdateScheduleStatusPayload) => {
+  return await getRequest().get<T.GetSchedulesResponse>(uri.schedule);
+};
+
+export const updateScheduleApi = async (
+  access_token: string,
+  payload: T.UpdateScheduleStatusPayload
+) => {
   try {
     const request = getRequest();
-  
-    await request.patch(uri.schedule , payload, {
-    headers: authorization(getAccessToken()),
-  });
+
+    await request.patch(uri.schedule, payload, {
+      headers: authorization(getAccessToken()),
+    });
   } catch (error) {
     throw error;
   }
-}
+};

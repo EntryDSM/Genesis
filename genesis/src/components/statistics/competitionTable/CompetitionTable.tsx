7a240720660f25fe495eb306992e87ae4f89dd6c
kimuichan <leerabi577@gmail.com>
2022-10-18 11:46:33 +0900

    순서 배치 바꿈

--- genesis/src/components/statistics/competitionTable/CompetitionTable.tsx
+++ genesis/src/components/statistics/competitionTable/CompetitionTable.tsx
@@ -58,57 +58,47 @@ const CompetitionTable: FC<Props> = ({ score }) => {
 
           <CommonScoreList
             type="158~170"
-            score1={score[0]["158-170"]}
-            score2={score[1]["158-170"]}
+            score1={score[1]["158-170"]}
+            score2={score[0]["158-170"]}
           />
           <CommonScoreList
             type="145~157"
-            score1={score[0]["145-157"]}
-            score2={score[1]["145-157"]}
+            score1={score[1]["145-157"]}
+            score2={score[0]["145-157"]}
           />
           <CommonScoreList
             type="132~144"
-            score1={score[0]["132-144"]}
-            score2={score[1]["132-144"]}
+            score1={score[1]["132-144"]}
+            score2={score[0]["132-144"]}
           />
           <CommonScoreList
             type="119~131"
-            score1={score[0]["119-131"]}
-            score2={score[1]["119-131"]}
+            score1={score[1]["119-131"]}
+            score2={score[0]["119-131"]}
           />
           <CommonScoreList
             type="106~118"
-            score1={score[0]["106-118"]}
-            score2={score[1]["106-118"]}
+            score1={score[1]["106-118"]}
+            score2={score[0]["106-118"]}
           />
           <CommonScoreList
             type="93~105"
-            score1={score[0]["93-105"]}
-            score2={score[1]["93-105"]}
+            score1={score[1]["93-105"]}
+            score2={score[0]["93-105"]}
           />
           <CommonScoreList
             type="80~92"
-            score1={score[0]["80-92"]}
-            score2={score[1]["80-92"]}
+            score1={score[1]["80-92"]}
+            score2={score[0]["80-92"]}
           />
           <CommonScoreList
             type="79 이하"
-            score1={score[0]["-79"]}
-            score2={score[1]["-79"]}
+            score1={score[1]["-79"]}
+            score2={score[0]["-79"]}
           />
           <CommonScoreList
             type="총 합"
             score1={
-              score[0]["158-170"] +
-              score[0]["145-157"] +
-              score[0]["132-144"] +
-              score[0]["119-131"] +
-              score[0]["106-118"] +
-              score[0]["93-105"] +
-              score[0]["80-92"] +
-              score[0]["-79"]
-            }
-            score2={
               score[1]["158-170"] +
               score[1]["145-157"] +
               score[1]["132-144"] +
@@ -117,6 +107,16 @@ const CompetitionTable: FC<Props> = ({ score }) => {
               score[1]["93-105"] +
               score[1]["80-92"] +
               score[1]["-79"]
+            }
+            score2={
+              score[0]["158-170"] +
+              score[0]["145-157"] +
+              score[0]["132-144"] +
+              score[0]["119-131"] +
+              score[0]["106-118"] +
+              score[0]["93-105"] +
+              score[0]["80-92"] +
+              score[0]["-79"]
             }
           />
           <tr className="table-edge"></tr>
@@ -136,73 +136,63 @@ const CompetitionTable: FC<Props> = ({ score }) => {
           </tr>
           <MeisterScoreList
             type="98~110"
-            score1={score[2]["98-110"]}
-            score2={score[3]["98-110"]}
-            score3={score[4]["98-110"]}
-            score4={score[5]["98-110"]}
+            score1={score[3]["98-110"]}
+            score2={score[2]["98-110"]}
+            score3={score[5]["98-110"]}
+            score4={score[4]["98-110"]}
           />
           <MeisterScoreList
             type="85~79"
-            score1={score[2]["85-97"]}
-            score2={score[3]["85-97"]}
-            score3={score[4]["85-97"]}
-            score4={score[5]["85-97"]}
+            score1={score[3]["85-97"]}
+            score2={score[2]["85-97"]}
+            score3={score[5]["85-97"]}
+            score4={score[4]["85-97"]}
           />
           <MeisterScoreList
             type="72~84"
-            score1={score[2]["72-84"]}
-            score2={score[3]["72-84"]}
-            score3={score[4]["72-84"]}
-            score4={score[5]["72-84"]}
+            score1={score[3]["72-84"]}
+            score2={score[2]["72-84"]}
+            score3={score[5]["72-84"]}
+            score4={score[4]["72-84"]}
           />
           <MeisterScoreList
             type="59~71"
-            score1={score[2]["59-71"]}
-            score2={score[3]["59-71"]}
-            score3={score[4]["59-71"]}
-            score4={score[5]["59-71"]}
+            score1={score[3]["59-71"]}
+            score2={score[2]["59-71"]}
+            score3={score[5]["59-71"]}
+            score4={score[4]["59-71"]}
           />
           <MeisterScoreList
             type="46~58"
-            score1={score[2]["46-58"]}
-            score2={score[3]["46-58"]}
-            score3={score[4]["46-58"]}
-            score4={score[5]["46-58"]}
+            score1={score[3]["46-58"]}
+            score2={score[2]["46-58"]}
+            score3={score[5]["46-58"]}
+            score4={score[4]["46-58"]}
           />
           <MeisterScoreList
             type="33~45"
-            score1={score[2]["33-45"]}
-            score2={score[3]["33-45"]}
-            score3={score[4]["33-45"]}
-            score4={score[5]["33-45"]}
+            score1={score[3]["33-45"]}
+            score2={score[2]["33-45"]}
+            score3={score[5]["33-45"]}
+            score4={score[4]["33-45"]}
           />
           <MeisterScoreList
             type="20~32"
-            score1={score[2]["20-32"]}
-            score2={score[3]["20-32"]}
-            score3={score[4]["20-32"]}
-            score4={score[5]["20-32"]}
+            score1={score[3]["20-32"]}
+            score2={score[2]["20-32"]}
+            score3={score[5]["20-32"]}
+            score4={score[4]["20-32"]}
           />
           <MeisterScoreList
             type="19 이하"
-            score1={score[2]["-19"]}
-            score2={score[3]["-19"]}
-            score3={score[4]["-19"]}
-            score4={score[5]["-19"]}
+            score1={score[3]["-19"]}
+            score2={score[2]["-19"]}
+            score3={score[5]["-19"]}
+            score4={score[4]["-19"]}
           />
           <MeisterScoreList
             type="총 합"
             score1={
-              score[2]["98-110"] +
-              score[2]["85-97"] +
-              score[2]["72-84"] +
-              score[2]["59-71"] +
-              score[2]["46-58"] +
-              score[2]["33-45"] +
-              score[2]["20-32"] +
-              score[2]["-19"]
-            }
-            score2={
               score[3]["98-110"] +
               score[3]["85-97"] +
               score[3]["72-84"] +
@@ -212,17 +202,17 @@ const CompetitionTable: FC<Props> = ({ score }) => {
               score[3]["20-32"] +
               score[3]["-19"]
             }
-            score3={
-              score[4]["98-110"] +
-              score[4]["85-97"] +
-              score[4]["72-84"] +
-              score[4]["59-71"] +
-              score[4]["46-58"] +
-              score[4]["33-45"] +
-              score[4]["20-32"] +
-              score[4]["-19"]
+            score2={
+              score[2]["98-110"] +
+              score[2]["85-97"] +
+              score[2]["72-84"] +
+              score[2]["59-71"] +
+              score[2]["46-58"] +
+              score[2]["33-45"] +
+              score[2]["20-32"] +
+              score[2]["-19"]
             }
-            score4={
+            score3={
               score[5]["98-110"] +
               score[5]["85-97"] +
               score[5]["72-84"] +
@@ -231,6 +221,16 @@ const CompetitionTable: FC<Props> = ({ score }) => {
               score[5]["33-45"] +
               score[5]["20-32"] +
               score[5]["-19"]
+            }
+            score4={
+              score[4]["98-110"] +
+              score[4]["85-97"] +
+              score[4]["72-84"] +
+              score[4]["59-71"] +
+              score[4]["46-58"] +
+              score[4]["33-45"] +
+              score[4]["20-32"] +
+              score[4]["-19"]
             }
           />
           <tr className="table-edge"></tr>
